9940e1cf5c994643c0399c0f6842b83d6eda8d7c
Ejike Onwe <ejikeonwe@Ejikes-MacBook-Pro.local>
2017-12-21 16:15:22 -0500

    Chart label issues resolved

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,7 +134,7 @@ require(['moment', 'chartjs'], function(moment, Chart) {
 
         type: 'bar',
         data: {
-            labels: ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10", "Week 11", "Week 12", "Week 13", "Week 14", "Week 15", "Week 16", "Week 17", "Week 18", "Week 19", "Week 20", "Week 21", "Week 22", "Week 23", "Week 24", "Week 25", "Week 26", "Week 27", "Week 28", "Week 29", "Week 30", "Week 31", "Week 32"],
+            labels: ["Week 1 ", "Week 2 ", "Week 3 ", "Week 4 ", "Week 5 ", "Week 6 ", "Week 7 ", "Week 8 ", "Week 9 ", "Week 10", "Week 11", "Week 12", "Week 13", "Week 14", "Week 15", "Week 16", "Week 17", "Week 18", "Week 19", "Week 20", "Week 21", "Week 22", "Week 23", "Week 24", "Week 25", "Week 26", "Week 27", "Week 28", "Week 29", "Week 30", "Week 31", "Week 32"],
             datasets: [{
                 label: 'Hours Per Week',
                 backgroundColor: '#b2c7c8',
